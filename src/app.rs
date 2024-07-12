638f12f799aa5a691d773f4f1de027db7594aaf2
Elon Aseneka <elonsantos63@gmail.com>
2024-07-12 20:06:40 +0300

    extend launch date

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,11 @@
-use yew::prelude::*;
-use std::time::Duration;
 use chrono::{TimeZone, Utc};
 use gloo::timers::callback::Interval;
+use std::time::Duration;
+use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let target_date = Utc.with_ymd_and_hms(2024, 7, 11, 17, 0, 0).unwrap();
+    let target_date = Utc.with_ymd_and_hms(2024, 7, 14, 17, 0, 0).unwrap();
     let now = Utc::now();
 
     let remaining_duration = if target_date > now {
@@ -19,18 +19,21 @@ pub fn app() -> Html {
     // Set interval to update countdown every second
     {
         let time_left = time_left.clone();
-        use_effect_with_deps(move |_| {
-            let interval = Interval::new(1000, move || {
-                let new_now = Utc::now();
-                let new_remaining_duration = if target_date > new_now {
-                    (target_date - new_now).to_std().unwrap()
-                } else {
-                    Duration::new(0, 0)
-                };
-                time_left.set(new_remaining_duration);
-            });
-            move || drop(interval)
-        }, ());
+        use_effect_with_deps(
+            move |_| {
+                let interval = Interval::new(1000, move || {
+                    let new_now = Utc::now();
+                    let new_remaining_duration = if target_date > new_now {
+                        (target_date - new_now).to_std().unwrap()
+                    } else {
+                        Duration::new(0, 0)
+                    };
+                    time_left.set(new_remaining_duration);
+                });
+                move || drop(interval)
+            },
+            (),
+        );
     }
 
     let days = time_left.as_secs() / 86400;
@@ -41,7 +44,7 @@ pub fn app() -> Html {
     html! {
         <main class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
             <img class="w-48 mb-8 animate-pulse text-gray-100" src="https://imagedelivery.net/fa3SWf5GIAHiTnHQyqU8IQ/de1ec98f-ab67-41b2-75af-7b9cc7d35200/public" alt="RT logo" />
-            <h1 class="text-5xl font-bold mb-4">{ "Launching Soon!" }</h1>
+            <h1 class="text-5xl font-bold mb-4">{ "Almost there! It will be worth the wait!" }</h1>
             <div class="text-3xl font-medium mb-8 w-64 text-gray-100">
                 { format!("{:02}d {:02}h {:02}m {:02}s", days, hours, minutes, seconds) }
             </div>
